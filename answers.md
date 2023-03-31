5ade3adc0c1f0a72003ad1e86c2725a5443579e1
anan <93016300+ananyaanand645@users.noreply.github.com>
2023-03-31 01:29:46 +0000

    Finished main.py and answers.md

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Recitation 6
 ## Answers
 
-**Name:**_________________________
+**Name:** Ananya Anand
 
 
 Place all written answers from `recitation-06.md` here for easier grading.
@@ -12,15 +12,19 @@ Place all written answers from `recitation-06.md` here for easier grading.
 
 File | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length
 ----------------------------------------------------------------------
-f1.txt    |                     |                |
-alice29.txt    |                     |                |
-asyoulik.txt    |                     |                |
-grammar.lsp    |                     |                |
-fields.c    |                     |                |
+f1.txt |   1340  |    826  | 0.61
+alice29.txt  |  1039367 | 676374 | 0.65
+asyoulik.txt |  876253 |  606448 | 0.69
+grammar.lsp | 26047 | 17356 | 0.67
+fields.c    | 78050|56206  | 0.72
 
 
 
 
 - **e.**
+- If every character in a document has the same frequency, then the Huffman encoding for the document would be a binary tree with all leaves at the same level.As a result, the Huffman algorithm would greedily combine the two least frequent characters into a single node until all characters are combined into a single node at the root of the tree. This tree would have $n$ leaf nodes.
+
+Therefore, the expected cost of a Huffman encoding for a document in which every character has the same frequency is the same as the cost of encoding a single character using a Huffman code with a binary tree of depth 1, which is 1. 
+
 
 
--- main.py
+++ main.py
@@ -24,38 +24,56 @@ def get_frequencies(fname):
 # given a dictionary f mapping characters to frequencies, 
 # create a prefix code tree using Huffman's algorithm
 def make_huffman_tree(f):
-    p = queue.PriorityQueue()
-    # construct heap from frequencies, the initial items should be
-    # the leaves of the final tree
-    for c in f.keys():
-        p.put(TreeNode(None,None,(f[c], c)))
+  p = queue.PriorityQueue()
+  # construct heap from frequencies, the initial items should be
+  # the leaves of the final tree
+  for c in f.keys():
+    p.put(TreeNode(None,None,(f[c],c)))
+
 
     # greedily remove the two nodes x and y with lowest frequency,
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
-    while (p.qsize() > 1):
-        # TODO
-        
-    # return root of the tree
-    return p.get()
+    
+  while (p.qsize() > 1):
+      y = p.get()
+      x = p.get()
+      z = TreeNode(x,y,(x.data[0] + y.data[0],""))
+      p.put(z)
+      
+  return p.get()
 
 # perform a traversal on the prefix code tree to collect all encodings
 def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+  if not(node.left and node.right):
+      code[node.data[1]] = prefix
+  else:
+    get_code(node.left, prefix + "0",code)
+    get_code(node.right, prefix + "1",code)
+  return code
+  
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
+    increment1 = 0
+    bitsval = math.ceil(math.log2(len(f)))
+    for i in f.values():
+      increment1 = increment1 + (i*bitsval)
+    return increment1
+    
 
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
-    # TODO
-    pass
+  cost = 0 
+  for char1, bin1 in C.items():
+      cost = cost + (len(bin1)*f[char1])
 
-f = get_frequencies('f1.txt')
+  return cost
+f = get_frequencies('fields.c')
+print(f)
 print("Fixed-length cost:  %d" % fixed_length_cost(f))
 T = make_huffman_tree(f)
+print(T)
 C = get_code(T)
+print(C)
 print("Huffman cost:  %d" % huffman_cost(C, f))
